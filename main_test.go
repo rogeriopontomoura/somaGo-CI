6cfc26fbbd51b6b4c4ea9bbc830a66419d2bd170
Rogerio <rogeriopontomoura@gmail.com>
2019-07-06 21:25:43 +0000

    Teste refatorado

--- main_test.go
+++ main_test.go
@@ -6,5 +6,12 @@ import "testing"
 
 // Funçaõ de teste
 func TestSoma(t *testing.T) {
+
+	resultado := soma(5,5)
+
+	if resultado != 10 {
+		t.Errorf("A soma está incorreta, retornou %d, quando deveria retornar %d.", resultado, 10)
+	}
+
 	
 }
